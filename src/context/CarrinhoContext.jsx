4869abf597c723dbc1597e8f5848dbe4bfb8a3f3
Kevin BTA <kevinleitesolar@hotmail.com>
2024-12-19 17:30:02 -0300

    Colocando nosso context api em outros componentes para o funcionamento correto do carrinho

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,13 +1,26 @@
 import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = "Carrinho";
 
-export const CarrinhoProvider = ({children}) => {
-   const [carrinho, setCarrinho] = useState([]);//Tiramos o use State de Home e de qualquer outro arquivo que estava usando, pois quem vai gerenciar o estado vai ser o Carrinho, que tem o contexto.
+export const CarrinhoProvider = ({ children }) => {
+	const [carrinho, setCarrinho] = useState([]); //Tiramos o use State de Home e de qualquer outro arquivo que estava usando, pois quem vai gerenciar o estado vai ser o Carrinho, que tem o contexto.
+	const [quantidade, setQuantidade] = useState(0);
+	const [valorTotal, setValorTotal] = useState(0);
 
-   return (
-      <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
-         {children}{/* Utilizamos o children, pois o Carrinho context vai englobar todo nosso app */}
-      </CarrinhoContext.Provider>
-   )
-}
+	return (
+		<CarrinhoContext.Provider
+			value={{
+				carrinho,
+				setCarrinho,
+				quantidade,
+				setQuantidade,
+				valorTotal,
+				setValorTotal,
+			}}
+		>
+			{children}
+			{/* Utilizamos o children, pois o Carrinho context vai englobar todo nosso app */}
+		</CarrinhoContext.Provider>
+	);
+};
--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,15 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 export const useCarrinhoContext = () => {
-	const { carrinho, setCarrinho } = useContext(CarrinhoContext); //usando o hook para pegar meu contexto e desestruturando os meus estados.
+	const {
+		carrinho,
+		setCarrinho,
+		quantidade,
+		setQuantidade,
+		valorTotal,
+		setValorTotal,
+	} = useContext(CarrinhoContext); //usando o hook para pegar meu contexto e desestruturando os meus estados.
 
-   function mudarQuantidade(id, quantidade) {
-      return carrinho.map((itemDoCarrinho) => {
-         if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
-         return itemDoCarrinho;
-      })
-   }
+	function mudarQuantidade(id, quantidade) {
+		return carrinho.map((itemDoCarrinho) => {
+			if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
+			return itemDoCarrinho;
+		});
+	}
 
 	function adicionarProduto(novoProduto) {
 		const temProduto = carrinho.some((itemDoCarrinho) => {
@@ -25,7 +32,7 @@ export const useCarrinhoContext = () => {
 			]);
 		}
 
-      const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1);
+		const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1);
 
 		setCarrinho([...carrinhoAtualizado]);
 	}
@@ -43,23 +50,43 @@ export const useCarrinhoContext = () => {
 			);
 		}
 
-      const carrinhoAtualizado = mudarQuantidade(id, -1);
+		const carrinhoAtualizado = mudarQuantidade(id, -1);
 
 		setCarrinho([...carrinhoAtualizado]);
 	}
 
-   function removerProdutoCarrinho(id) {
-      // Aqui utilizaremos o filter, para criar um  novo array com todos os produtos na lista do carrinho, assim vamos fazer uma função que vai retornar(receber/ter) a comparação do id do item do carrinho com o id do parametro, retornando o que não for igual ao que já está lá, ou seja, retirando ele da lista, assim fazendo a função de excluir.
-      // Basicamente ele vai comparar o id que foi passado como parametro atraves do click no item, com o id do item atual, se for igual, ele vai retornar todos os itens anteriores, menos o que for igual
-      const produto = carrinho.filter((itemDoCarrinho) => itemDoCarrinho.id !== id);
-      setCarrinho(produto)
-   }
+	function removerProdutoCarrinho(id) {
+		// Aqui utilizaremos o filter, para criar um  novo array com todos os produtos na lista do carrinho, assim vamos fazer uma função que vai retornar(receber/ter) a comparação do id do item do carrinho com o id do parametro, retornando o que não for igual ao que já está lá, ou seja, retirando ele da lista, assim fazendo a função de excluir.
+		// Basicamente ele vai comparar o id que foi passado como parametro atraves do click no item, com o id do item atual, se for igual, ele vai retornar todos os itens anteriores, menos o que for igual
+		const produto = carrinho.filter(
+			(itemDoCarrinho) => itemDoCarrinho.id !== id
+		);
+		setCarrinho(produto);
+	}
+
+	useEffect(() => {
+		const { totalTemp, quantidadeTemp } = carrinho.reduce(
+			(acumulador, produto) => ({
+				quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+				totalTemp:
+					acumulador.totalTemp + produto.preco * produto.quantidade,
+			}),
+         {
+            quantidadeTemp: 0,
+            totalTemp: 0,
+         }
+		);
+      setQuantidade(quantidadeTemp);
+      setValorTotal(totalTemp);
+	}, [carrinho]);
 
 	return {
 		carrinho,
 		setCarrinho,
 		adicionarProduto,
-      removerProduto,
-      removerProdutoCarrinho,
+		removerProduto,
+		removerProdutoCarrinho,
+      valorTotal,
+      quantidade,
 	};
 };
